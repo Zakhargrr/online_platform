24c71e73a1f96e342677d49ef2f3fc28aee087e1
zakhar <grankin-zakhar@mail.ru>
2024-01-20 03:09:54 +0700

    fixed csu.py

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,13 +5,15 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(
-            email='admin@mail.ru',
-            first_name='Admin',
-            last_name='Admin',
-            is_staff=True,
-            is_superuser=True
-        )
+        db_user = User.objects.filter(email='admin@mail.ru')
+        if not db_user:
+            user = User.objects.create(
+                email='admin@mail.ru',
+                first_name='Admin',
+                last_name='Admin',
+                is_staff=True,
+                is_superuser=True
+            )
 
-        user.set_password('12345')
-        user.save()
+            user.set_password('12345')
+            user.save()
